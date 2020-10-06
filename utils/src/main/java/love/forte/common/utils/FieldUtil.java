7220961ee0cec91c42bf926250f986dc4ef6e646
ForteScarlet <ForteScarlet@163.com>
2020-10-06 21:09:56 +0800

    msgs.

--- utils/src/main/java/love/forte/common/utils/FieldUtil.java
+++ utils/src/main/java/love/forte/common/utils/FieldUtil.java
@@ -13,10 +13,10 @@
 package love.forte.common.utils;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 获取字段的工具类
@@ -79,6 +79,74 @@ public class FieldUtil {
             return fieldList;
         }
     }
+
+
+    /**
+     * 提供一个目标列表，和一个查询类型，查找这个类型中实现了的目标列表中的接口数量。
+     * 例如：
+     * {@code class A implements B, C}，
+     * {@code interface B extends D}，
+     *
+     * 则可以认为 {@code A} 命中了 {@code B, C, D}。
+     *
+     * @param type 查找目标。
+     * @param targets 寻找的目标接口。类型应当都是接口类型，否则没有意义。
+     * @return
+     */
+    public static Set<Class<?>> getHitSuperInterfaces(Class<?> type, Set<Class<?>> targets) {
+        return allInterfacesStream(type).distinct()
+                // 寻找所有targets中的子类型。
+                .flatMap(c -> targets.stream().filter(tc -> tc.isAssignableFrom(c)))
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * 提供一个目标列表，和一个查询类型，查找这个类型中实现了的目标列表中的接口数量。
+     * 例如：
+     * {@code class A implements B, C}，
+     * {@code interface B extends D}，
+     *
+     * 则可以认为 {@code A} 命中了 {@code B, C, D}。
+     *
+     * 会出现重复的情况，需要注意去重。
+     *
+     * @param type 查找目标。
+     * @param targets 寻找的目标接口。类型应当都是接口类型，否则没有意义。
+     */
+    public static Stream<Class<?>> getHitSuperInterfacesStream(Class<?> type, Set<Class<?>> targets) {
+        return allInterfacesStream(type).distinct()
+                // 寻找所有targets中的子类型。
+                .flatMap(c -> targets.stream().filter(tc -> tc.isAssignableFrom(c)));
+    }
+
+
+    /**
+     * 获取一个类型所实现的所有接口。包括诸如父类等层级关系。
+     * @param type 某类型
+     * @return 所有接口。
+     */
+    public static Set<Class<?>> allInterfaces(Class<?> type) {
+        return allInterfacesStream(type).collect(Collectors.toSet());
+    }
+
+
+    /**
+     * 获取一个类型所实现的所有接口。包括诸如父类等层级关系。
+     *
+     * 会出现重复的情况，需要注意去重。
+     *
+     * @param type 某类型
+     * @return 所有接口。
+     */
+    public static Stream<Class<?>> allInterfacesStream(Class<?> type) {
+        final Class<?>[] interfaces = type.getInterfaces();
+        if(interfaces.length == 0) {
+            return Stream.empty();
+        }
+        return Arrays.stream(interfaces).flatMap(FieldUtil::allInterfacesStream);
+    }
+
+
 
 
 }
