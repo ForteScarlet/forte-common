32788293f0e707ef2ad5013bf124e2cb0f195e44
ForteScarlet <ForteScarlet@163.com>
2020-10-06 17:40:26 +0800

    neko some.

--- utils/src/main/java/love/forte/common/collections/_CollectionsEx.kt
+++ utils/src/main/java/love/forte/common/collections/_CollectionsEx.kt
@@ -11,6 +11,7 @@
  */
 
 @file:JvmName("CollectionsExpand")
+
 package love.forte.common.collections
 
 
@@ -18,5 +19,48 @@ package love.forte.common.collections
     为kotlin提供一些对于collections的额外的方法.
  */
 
+/**
+ * Simple Empty.
+ */
+data class SimpleEntry<K, V>(override val key: K, override val value: V) : Map.Entry<K, V>
+
+
+/**
+ * Array set, simple [Set] by [Array].
+ */
+data class ArraySet<T>(private val array: Array<T>) : Set<T> {
+
+    override val size: Int
+        get() = array.size
+
+    override fun contains(element: T): Boolean = array.contains(element)
+
+    override fun containsAll(elements: Collection<T>): Boolean = elements.all { contains(it) }
+
+    override fun isEmpty(): Boolean = array.isEmpty()
+
+    override fun iterator(): Iterator<T> = array.iterator()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ArraySet<*>
+
+        if (!array.contentEquals(other.array)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return array.contentHashCode()
+    }
+
+}
+
+/**
+ * array set of.
+ */
+public fun <T> arraySetOf(vararg value: T) : Set<T> = ArraySet(value)
 
 
