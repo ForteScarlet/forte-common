9075a43f5a27dda3551a88b1ddb65b13e2416b51
ForteScarlet <ForteScatlet@163.com>
2020-11-18 16:37:19 +0800

    AnnotationUtil 缓存变更为lru缓存

---
+++ utils/src/main/java/love/forte/common/collections/LRULinkedHashMap.kt
@@ -0,0 +1,41 @@
+package love.forte.common.collections
+
+
+/**
+ *
+ * Lru map 最基础的实现。
+ *
+ */
+public class LRULinkedHashMap<K, V> : LinkedHashMap<K, V> {
+    @Volatile
+    var capacity: Int
+
+    constructor(capacity: Int, initialCapacity: Int, loadFactor: Float) : super(initialCapacity, loadFactor, true) {
+        this.capacity = capacity
+    }
+
+    constructor(capacity: Int, initialCapacity: Int) : super(initialCapacity, 0.75f, true) {
+        this.capacity = capacity
+    }
+
+    constructor(capacity: Int) : super() {
+        this.capacity = capacity
+    }
+
+    constructor(capacity: Int, m: MutableMap<out K, out V>?) : super(m) {
+        this.capacity = capacity
+    }
+
+    constructor(capacity: Int, initialCapacity: Int, loadFactor: Float, accessOrder: Boolean) : super(
+        initialCapacity,
+        loadFactor,
+        accessOrder
+    ) {
+        this.capacity = capacity
+    }
+
+    override fun removeEldestEntry(eldest: MutableMap.MutableEntry<K, V>?): Boolean {
+        return size > capacity
+    }
+
+}
